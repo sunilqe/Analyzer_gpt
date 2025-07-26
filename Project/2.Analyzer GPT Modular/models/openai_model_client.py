8daf66299d155757170db96f7f352b847d5f7cfb
Sunil Kumar <sunilqe@gmail.com>
2025-07-26 22:00:29 +0530

    modular code updated

---
+++ Project/2.Analyzer GPT Modular/models/openai_model_client.py
@@ -0,0 +1,11 @@
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+import os
+from config.constants import MODEL_OPENAI
+
+def get_model_client():
+    openai_model_client = OpenAIChatCompletionClient(
+        model=MODEL_OPENAI
+        api_key=os.getenv('OPENAI_API_KEY')
+    )
+
+    return openai_model_client
